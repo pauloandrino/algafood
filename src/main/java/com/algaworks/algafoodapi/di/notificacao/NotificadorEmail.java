d8ad55eab720746da394b3dcdfeb2f04525d2afc
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-05-13 22:38:06 -0300

    Spring injection.

--- src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
@@ -1,11 +1,8 @@
 package com.algaworks.algafoodapi.di.notificacao;
 
 import com.algaworks.algafoodapi.di.model.Cliente;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("prod")
 @TipoNotificador(NivelUrgencia.URGENTE)
 @Component
 public class NotificadorEmail implements Notificador {
