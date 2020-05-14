43540354ff8b8a95983e6e61ab4e1ce1f05f9d6b
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-05-14 18:44:26 -0300

    JPA - Test class to get cozinha by id.

---
+++ leiame.md
@@ -0,0 +1,9 @@
+# Leiame
+
+
+###For developement only:
+
+#####Run mySql in docker:
+docker run --name algafood-sql -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_DATABASE=algafooddb -e TZ=America/Sao_Paulo -p 3306:3306 -d mysql:latest
+
+
---
+++ src/main/java/com/algaworks/algafoodapi/jpa/BuscaCozinhaMain.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafoodapi.jpa;
+
+import com.algaworks.algafoodapi.AlgafoodApiApplication;
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class BuscaCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext context = (ApplicationContext) new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = context.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha = cadastroCozinha.buscar(1L);
+
+        System.out.println(cozinha.getNome());
+
+    }
+}
