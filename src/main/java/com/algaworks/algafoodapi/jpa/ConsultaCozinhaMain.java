d40e69f25377549f0b423fe5877b199ca9e602bc
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-05-13 23:29:12 -0300

    JPA - Test class to list cozinha.

---
+++ src/main/java/com/algaworks/algafoodapi/jpa/ConsultaCozinhaMain.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafoodapi.jpa;
+
+import com.algaworks.algafoodapi.AlgafoodApiApplication;
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class ConsultaCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext context = (ApplicationContext) new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = context.getBean(CadastroCozinha.class);
+
+        List<Cozinha> cozinhas = cadastroCozinha.listar();
+
+        for (Cozinha cozinha : cozinhas) {
+            System.out.println(cozinha.getNome());
+        }
+
+    }
+}
