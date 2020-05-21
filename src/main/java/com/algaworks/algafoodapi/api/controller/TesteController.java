f9cb66d917666fba2763c086affc4b5789102f9c
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-05-21 20:42:34 -0300

    REST - Change repositories to @Respository.

---
+++ src/main/java/com/algaworks/algafoodapi/api/controller/TesteController.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafoodapi.api.controller;
+
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import com.algaworks.algafoodapi.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/teste")
+public class TesteController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping("/cozinhas/por-nome")
+    public List<Cozinha> cozonhasPorNome(@RequestParam String nome) {
+        return cozinhaRepository.consultarPorNome(nome);
+    }
+}
