cebc49441629acdf5f4e29f3648c6ec7959cabb6
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-05-24 19:22:34 -0300

    JPA - Query methods.

--- src/main/java/com/algaworks/algafoodapi/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafoodapi/domain/repository/CozinhaRepository.java
@@ -4,8 +4,10 @@ import com.algaworks.algafoodapi.domain.model.Cozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
-//    public List<Cozinha> consultarPorNome(String nome);
+    public List<Cozinha> findByNome(String nome);
 }
