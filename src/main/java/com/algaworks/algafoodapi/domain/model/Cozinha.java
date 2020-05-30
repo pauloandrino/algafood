6ac3dc7b1dae400498813408c45ae036ae0d0415
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-05-30 09:57:18 -0300

    JPA - Bidirectional mapping - using mapped by.

--- src/main/java/com/algaworks/algafoodapi/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Cozinha.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafoodapi.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -9,6 +10,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -22,6 +26,11 @@ public class Cozinha {
 
     @Column(nullable = false)
     private String nome;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cozinha")
+    private List<Restaurante> restaurantes = new ArrayList<>();
+
 
 
 }
