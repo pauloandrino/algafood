26240b835af6b69f8e03077b4300e808ca9055b2
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-05-30 10:39:06 -0300

    JPA - ManyToMany mapping.

--- src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -8,8 +8,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -29,4 +34,10 @@ public class Restaurante {
 
     @ManyToOne
     private Cozinha cozinha;
+
+    @ManyToMany
+    @JoinTable(name = "restaurante_forma_pagamento",
+            joinColumns = @JoinColumn(name = "restaurante_id"),
+            inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+    private List<FormaPagamento> formasPagamento = new ArrayList<>();
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,7 +7,6 @@ insert into restaurante (id, nome, taxa_frete, cozinha_id) values (1, 'Thai Gour
 insert into restaurante (id, nome, taxa_frete, cozinha_id) values (2, 'Thai Delivery', 9.90, 1);
 insert into restaurante (id, nome, taxa_frete, cozinha_id) values (3, 'Tuk Tuk Indiana', 2.90, 2);
 
-
 insert into estado (id, nome) values (1, "São Paulo");
 insert into estado (id, nome) values (2, "Minas Gerais");
 
@@ -15,3 +14,12 @@ insert into cidade (id, nome, estado_id) values (1, "Sorocaba", 1);
 insert into cidade (id, nome, estado_id) values (2, "São Paulo", 1);
 insert into cidade (id, nome, estado_id) values (3, "Belo Horizone", 2);
 insert into cidade (id, nome, estado_id) values (4, "Monte Verde", 2);
+
+insert into forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
+insert into forma_pagamento (id, descricao) values (2, 'Cartão de débito');
+insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
+
+insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
+insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
