c94d88d2866701fc82856837d624758cb461b8f2
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-06-08 19:08:19 -0300

    Bean Validation - Custom message using the annotation message property.

--- src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -38,7 +38,7 @@ public class Restaurante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
+    @NotBlank(message = "Nome é obrigatório")
     @Column
     private String nome;
 
