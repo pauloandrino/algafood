fef09f0a6e32ad543d023d19d64c6581c5ef15db
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-06-08 17:57:14 -0300

    Code clean up.

--- src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -1,7 +1,6 @@
 package com.algaworks.algafoodapi.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
@@ -10,7 +9,6 @@ import org.hibernate.annotations.UpdateTimestamp;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -18,10 +16,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -38,20 +33,16 @@ public class Restaurante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @NotNull
-//    @NotEmpty
     @NotBlank
     @Column
     private String nome;
 
-//    @DecimalMin("0")
     @PositiveOrZero
     @Column(nullable = false)
     private BigDecimal taxaFrete;
 
-//    @JsonIgnore
-    @JsonIgnoreProperties("hibernateLazyInitializer")
-    @ManyToOne //(fetch = FetchType.LAZY)
+
+    @ManyToOne
     private Cozinha cozinha;
 
     @JsonIgnore
