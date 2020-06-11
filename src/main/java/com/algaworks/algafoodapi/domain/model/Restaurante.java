6d87cc3eb1c12f6362c3f866b89bb5e276430435
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-06-11 12:20:38 -0300

    Bean Validation - Create a custom annotation to create a validation constrain.

--- src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafoodapi.domain.model;
 
 import com.algaworks.algafoodapi.core.validation.Groups;
+import com.algaworks.algafoodapi.core.validation.TaxaFrete;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -43,7 +44,8 @@ public class Restaurante {
     private String nome;
 
     @NotNull
-    @PositiveOrZero()
+//    @PositiveOrZero()
+    @TaxaFrete
     @Column(nullable = false)
     private BigDecimal taxaFrete;
 
