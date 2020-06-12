fed09ce6d723cacb0dd119a9fc8e074dae3777cb
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-06-11 21:02:36 -0300

    Integration Tests - Add maven-failsafe-plugin in order to NOT run integration test on maven builds. In order to run integration test use "mvn verify".

--- src/test/java/com/algaworks/algafoodapi/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafoodapi/CadastroCozinhaIT.java
@@ -17,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CadastroCozinhaIntegrationTest {
+public class CadastroCozinhaIT {
 
 	@Autowired
 	private CadastroCozinhaService cadastroCozinha;
