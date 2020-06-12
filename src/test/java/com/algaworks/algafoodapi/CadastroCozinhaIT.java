62fe00fc509073cd036f0090d0d97ea28982de84
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-06-11 21:04:32 -0300

    Integration Tests - Removing integration tests - From now on, we will do API tests.

--- src/test/java/com/algaworks/algafoodapi/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafoodapi/CadastroCozinhaIT.java
@@ -1,62 +1,12 @@
 package com.algaworks.algafoodapi;
 
-import com.algaworks.algafoodapi.domain.exception.EntidadeEmUsoException;
-import com.algaworks.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
-import com.algaworks.algafoodapi.domain.model.Cozinha;
-import com.algaworks.algafoodapi.domain.service.CadastroCozinhaService;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.validation.ConstraintViolationException;
-
-import static org.junit.Assert.assertFalse;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CadastroCozinhaIT {
 
-	@Autowired
-	private CadastroCozinhaService cadastroCozinha;
-
-	@Test
-	public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
-
-		// cenário
-		Cozinha novaCozinha = new Cozinha();
-		novaCozinha.setNome("Chinesa");
-
-		// ação
-		novaCozinha = cadastroCozinha.salvar(novaCozinha);
-
-		// validação
-		assertThat(novaCozinha).isNotNull();
-		assertThat(novaCozinha.getId()).isNotNull();
-
-	}
-
-	@Test(expected = ConstraintViolationException.class)
-	public void deveFalhar_QuandoCadastrarCozinhaSemNome() {
-
-		// cenário
-		Cozinha novaCozinha = new Cozinha();
-		novaCozinha.setNome(null);
-
-		// ação
-		novaCozinha = cadastroCozinha.salvar(novaCozinha);
-	}
-
-	@Test(expected = EntidadeEmUsoException.class)
-	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
-		cadastroCozinha.excluir(1L);
-	}
-
-	@Test(expected = EntidadeNaoEncontradaException.class)
-	public void deveFalhar_QuandoExcluirCozinhaInexistente() {
-		cadastroCozinha.excluir(10L);
-	}
 
 }
