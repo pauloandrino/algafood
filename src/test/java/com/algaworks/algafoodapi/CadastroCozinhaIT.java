3c78efaf8b6bca1a8a04e1e82e287a3572ffbec4
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-06-11 21:22:43 -0300

    Integration Tests - API Test - Using REST Assured to test the request/response. Validating the status code.

--- src/test/java/com/algaworks/algafoodapi/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafoodapi/CadastroCozinhaIT.java
@@ -1,12 +1,37 @@
 package com.algaworks.algafoodapi;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static io.restassured.RestAssured.given;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CadastroCozinhaIT {
 
+    @LocalServerPort
+    private int port;
+
+
+    @Test
+    public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        given()
+            .basePath("/cozinhas")
+            .port(port)
+            .accept(ContentType.JSON)
+        .when()
+            .get()
+        .then()
+            .statusCode(HttpStatus.OK.value());
+    }
 
 }
