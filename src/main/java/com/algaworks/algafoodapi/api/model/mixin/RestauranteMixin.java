1f7c68b3fbfb1d68de106cfa01fe47e5fdc246ef
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-06-13 20:50:05 -0300

    Best Practices - Use mixin classes for Jackson annotations. In that way, jackson configurations will be outside of domain class.

---
+++ src/main/java/com/algaworks/algafoodapi/api/model/mixin/RestauranteMixin.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafoodapi.api.model.mixin;
+
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import com.algaworks.algafoodapi.domain.model.Endereco;
+import com.algaworks.algafoodapi.domain.model.FormaPagamento;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestauranteMixin {
+
+    @JsonIgnoreProperties(value = "nome", allowGetters = true)
+    private Cozinha cozinha;
+
+    @JsonIgnore
+    private Endereco endereco;
+
+    @JsonIgnore
+    private LocalDateTime dataCadastro;
+
+    @JsonIgnore
+    private LocalDateTime dataAtualizacao;
+
+    @JsonIgnore
+    private List<FormaPagamento> formasPagamento = new ArrayList<>();
+}
--- src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -2,8 +2,6 @@ package com.algaworks.algafoodapi.domain.model;
 
 import com.algaworks.algafoodapi.core.validation.Groups;
 import com.algaworks.algafoodapi.core.validation.ValorZeroIncluiDescricao;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
@@ -51,28 +49,23 @@ public class Restaurante {
     private BigDecimal taxaFrete;
 
 
-    @JsonIgnoreProperties(value = "nome", allowGetters = true)
     @Valid
     @ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
     @NotNull
     @ManyToOne
     private Cozinha cozinha;
 
-    @JsonIgnore
     @Embedded
     private Endereco endereco;
 
-    @JsonIgnore
     @CreationTimestamp
     @Column(nullable = false, columnDefinition = "datetime")
     private LocalDateTime dataCadastro;
 
-    @JsonIgnore
     @UpdateTimestamp
     @Column(nullable = false, columnDefinition = "datetime")
     private LocalDateTime dataAtualizacao;
 
-    @JsonIgnore
     @ManyToMany
     @JoinTable(name = "restaurante_forma_pagamento",
             joinColumns = @JoinColumn(name = "restaurante_id"),
