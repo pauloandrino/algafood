df2a512e5efd9e67cedfed292a29b5da7d95986c
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-06-14 19:24:53 -0300

    Best Practices - Added ModelMapper.

--- src/main/java/com/algaworks/algafoodapi/api/assembler/RestauranteInputDisasembler.java
+++ src/main/java/com/algaworks/algafoodapi/api/assembler/RestauranteInputDisasembler.java
@@ -1,25 +1,19 @@
 package com.algaworks.algafoodapi.api.assembler;
 
 import com.algaworks.algafoodapi.api.model.input.RestauranteInput;
-import com.algaworks.algafoodapi.domain.model.Cozinha;
 import com.algaworks.algafoodapi.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestauranteInputDisasembler {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public Restaurante toDomainModel(RestauranteInput restauranteInput) {
-
-        Restaurante restaurante = new Restaurante();
-        restaurante.setNome(restauranteInput.getNome());
-        restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-
-        Cozinha cozinha = new Cozinha();
-        cozinha.setId(restauranteInput.getCozinha().getId());
-
-        restaurante.setCozinha(cozinha);
-
-        return restaurante;
+        return modelMapper.map(restauranteInput, Restaurante.class);
     }
 
 }
