57c630f49462b4d3b2cf1835ea9e6d139956a10f
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-06-17 18:19:17 -0300

    Advanced modeling - Modeling endereco model for restaurante resource.

--- src/main/java/com/algaworks/algafoodapi/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafoodapi/core/modelmapper/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafoodapi.core.modelmapper;
 
+import com.algaworks.algafoodapi.api.model.EnderecoModel;
+import com.algaworks.algafoodapi.domain.model.Endereco;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,16 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+
+        var modelMapper = new ModelMapper();
+
+        var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(
+                Endereco.class, EnderecoModel.class);
+
+        enderecoToEnderecoModelTypeMap.<String>addMapping(
+                enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+                (enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value));
+
+        return modelMapper;
     }
 }
