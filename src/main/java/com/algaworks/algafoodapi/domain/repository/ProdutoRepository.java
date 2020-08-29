08a72a4d37e79d3e024db3ec25b2cbee1cef465c
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-08-29 14:37:18 -0300

    Projections, Searches and Reports - Simple search.

--- src/main/java/com/algaworks/algafoodapi/domain/repository/ProdutoRepository.java
+++ src/main/java/com/algaworks/algafoodapi/domain/repository/ProdutoRepository.java
@@ -19,5 +19,7 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
     List<Produto> findByRestaurante(Restaurante restaurante);
 
+    @Query("from Produto p where p.ativo = true and p.restaurante = :restaurante")
+    List<Produto> findAtivosByRestaurante(Restaurante restaurante);
 
 }
