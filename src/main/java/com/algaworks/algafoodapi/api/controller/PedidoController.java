9df98207c4f7a4e45ccf2ba762f599ad5a9a312b
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-09-09 06:14:31 -0300

    Projections, Searches and Reports - Pagination and order.

--- src/main/java/com/algaworks/algafoodapi/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/PedidoController.java
@@ -15,6 +15,9 @@ import com.algaworks.algafoodapi.domain.repository.filter.PedidoFilter;
 import com.algaworks.algafoodapi.domain.service.EmissaoPedidoService;
 import com.algaworks.algafoodapi.infrastructure.repository.spec.PedidoSpecs;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,10 +50,15 @@ public class PedidoController {
     private PedidoInputDisassembler pedidoInputDisassembler;
 
     @GetMapping
-    public List<PedidoResumoModel> pesquisar(PedidoFilter filtro) {
-        List<Pedido> todosPedidos = pedidoRepository.findAll(PedidoSpecs.usandoFiltro(filtro));
+    public Page<PedidoResumoModel> pesquisar(PedidoFilter filtro, Pageable pageable) {
+        Page<Pedido> pedidosPage = pedidoRepository.findAll(PedidoSpecs.usandoFiltro(filtro), pageable);
 
-        return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
+        List<PedidoResumoModel> pedidosResumoModel = pedidoResumoModelAssembler
+                .toCollectionModel(pedidosPage.getContent());
+
+        Page<PedidoResumoModel> pedidosModelPage = new PageImpl<>(pedidosResumoModel, pageable,
+                pedidosPage.getTotalElements());
+        return pedidosModelPage;
     }
 
     @GetMapping("/{codigoPedido}")
