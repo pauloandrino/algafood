5aa8af2bdbf9a401634880e216d0a8746da29e6a
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-09-30 19:05:06 -0300

    Projections, Searches and Reports - Filling PDF report.

--- src/main/java/com/algaworks/algafoodapi/infrastructure/service/report/PdfVendaReportService.java
+++ src/main/java/com/algaworks/algafoodapi/infrastructure/service/report/PdfVendaReportService.java
@@ -1,14 +1,41 @@
 package com.algaworks.algafoodapi.infrastructure.service.report;
 
 import com.algaworks.algafoodapi.domain.filter.VendaDiariaFilter;
+import com.algaworks.algafoodapi.domain.service.VendaQueryService;
 import com.algaworks.algafoodapi.domain.service.VendaReportService;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Locale;
 
 @Service
 public class PdfVendaReportService implements VendaReportService {
 
+    @Autowired
+    private VendaQueryService vendaQueryService;
+
     @Override
     public byte[] emitirVendasDiasrias(VendaDiariaFilter filtro, String timeOffset) {
-        return null;
+
+        try {
+            var inputStream = this.getClass().getResourceAsStream("/relatorios/vendas-diarias.jasper");
+
+            var parameters = new HashMap<String, Object>();
+//            parameters.put("REPORT_LOCALE", new Locale("pt", "BR"));
+
+
+            var vendasDiarias = vendaQueryService.consultaVendasDiarias(filtro, timeOffset);
+            var dataSource = new JRBeanCollectionDataSource(vendasDiarias);
+
+            var jasperPrint = JasperFillManager.fillReport(inputStream, parameters, dataSource);
+
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+        } catch (Exception e) {
+            throw new ReportException("Não foi possível emitir relatório de vendas diárias", e);
+        }
     }
 }
