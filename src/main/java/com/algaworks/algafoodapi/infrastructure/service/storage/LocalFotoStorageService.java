d6e4740d117318514c4dc8ce5edf8e9b4ce23412
Paulo Andrino <pauloandrino@yahoo.com.br>
2020-10-25 18:15:49 -0300

    Projections, Searches and Reports - FotoProduto save - replace old photo when saving a new one.

--- src/main/java/com/algaworks/algafoodapi/infrastructure/service/storage/LocalFotoStorageService.java
+++ src/main/java/com/algaworks/algafoodapi/infrastructure/service/storage/LocalFotoStorageService.java
@@ -27,6 +27,16 @@ public class LocalFotoStorageService implements FotoStorageService {
         }
     }
 
+    @Override
+    public void remover(String nomeArquivo) {
+        try {
+            Path arquivoPath = getArquivoPath(nomeArquivo);
+            Files.deleteIfExists(arquivoPath);
+        } catch (IOException e) {
+            throw new StorageException("Não foi possível excluir arquivo", e);
+        }
+    }
+
     private Path getArquivoPath(String nomeArquivo) {
         return diretorioFotos.resolve(Path.of(nomeArquivo));
     }
