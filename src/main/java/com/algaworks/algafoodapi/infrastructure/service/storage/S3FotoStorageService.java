e0fb59517153f4e17eca67223432579436cf6945
Paulo Andrino <pauloandrino@yahoo.com.br>
2021-01-18 18:23:55 -0300

    Files Upload and Download - Adding S3 foto storage service.

---
+++ src/main/java/com/algaworks/algafoodapi/infrastructure/service/storage/S3FotoStorageService.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafoodapi.infrastructure.service.storage;
+
+import com.algaworks.algafoodapi.domain.service.FotoStorageService;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+
+@Service
+public class S3FotoStorageService implements FotoStorageService {
+    @Override
+    public InputStream recuperar(String nomeArquivo) {
+        return null;
+    }
+
+    @Override
+    public void armazenar(NovaFoto novaFoto) {
+
+    }
+
+    @Override
+    public void remover(String nomeArquivo) {
+
+    }
+}
