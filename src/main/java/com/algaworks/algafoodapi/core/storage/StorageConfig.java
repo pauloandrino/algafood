2b1e55c7e2220fcd067d3116423567284ba708a7
Paulo Andrino <pauloandrino@yahoo.com.br>
2021-01-25 17:42:22 -0300

    Files Upload and Download - Select storage type (Local or S3) via properties file.

--- src/main/java/com/algaworks/algafoodapi/core/storage/StorageConfig.java
+++ src/main/java/com/algaworks/algafoodapi/core/storage/StorageConfig.java
@@ -1,21 +1,26 @@
 package com.algaworks.algafoodapi.core.storage;
 
+import com.algaworks.algafoodapi.domain.service.FotoStorageService;
+import com.algaworks.algafoodapi.infrastructure.service.storage.LocalFotoStorageService;
+import com.algaworks.algafoodapi.infrastructure.service.storage.S3FotoStorageService;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AmazonS3Config {
+public class StorageConfig {
 
     @Autowired
     private StorageProperties storageProperties;
 
     @Bean
+    @ConditionalOnProperty(name = "algafood.storage.tipo", havingValue = "S3")
     public AmazonS3 amazonS3() {
         var credentials = new BasicAWSCredentials(
                 storageProperties.getS3().getIdChaveAcesso(),
@@ -30,5 +35,14 @@ public class AmazonS3Config {
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withPathStyleAccessEnabled(true)
                 .build();
+    }
+
+    @Bean
+    public FotoStorageService fotoStorageService() {
+        if(StorageProperties.TipoStorage.S3.equals(storageProperties.getTipo())) {
+            return new S3FotoStorageService();
+        } else {
+            return new LocalFotoStorageService();
+        }
     }
 }
