d81d27f28450be153a0f9a2aadc9b0361bd610dd
Paulo Andrino <pauloandrino@yahoo.com.br>
2021-08-19 09:56:47 -0300

    Transactions Email and Domain Events - Listening event - Using transaction phases to determine if it should be saved or not.

--- src/main/java/com/algaworks/algafoodapi/domain/listeners/NotificacaoClientePedidoConfirmadoListener.java
+++ src/main/java/com/algaworks/algafoodapi/domain/listeners/NotificacaoClientePedidoConfirmadoListener.java
@@ -4,8 +4,9 @@ import com.algaworks.algafoodapi.domain.event.PedidoConfirmadoEvent;
 import com.algaworks.algafoodapi.domain.service.EnvioEmailService;
 import com.algaworks.algafoodapi.domain.service.EnvioEmailService.Mensagem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class NotificacaoClientePedidoConfirmadoListener {
@@ -13,7 +14,8 @@ public class NotificacaoClientePedidoConfirmadoListener {
     @Autowired
     private EnvioEmailService envioEmail;
 
-    @EventListener
+    @TransactionalEventListener(/*phase = TransactionPhase.BEFORE_COMMIT*/) // Default - After commit, it's OK if the email fails
+                                                                            // Use Before COMMIT if the listener MUST be success
     public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
 
         System.out.println("Listener");
