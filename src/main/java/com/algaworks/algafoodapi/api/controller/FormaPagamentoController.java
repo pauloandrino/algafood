32ccc99b573fc25c6915210d392757435a2b3d41
Paulo Andrino <pauloandrino@yahoo.com.br>
2021-08-25 20:41:22 -0300

    CACHE - Using conditional requests with Deep ETags.

--- src/main/java/com/algaworks/algafoodapi/api/controller/FormaPagamentoController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/FormaPagamentoController.java
@@ -69,7 +69,23 @@ public class FormaPagamentoController {
     }
 
     @GetMapping("{formaPagamentoId}")
-    public ResponseEntity<FormaPagamentoModel> buscar(@PathVariable Long formaPagamentoId) {
+    public ResponseEntity<FormaPagamentoModel> buscar(@PathVariable Long formaPagamentoId, ServletWebRequest request) {
+
+        ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+
+        String eTag = "0";
+
+        OffsetDateTime dataAtualizacao = formaPagamentoRepository
+                .getDataAtualizacaoById(formaPagamentoId);
+
+        if (dataAtualizacao != null) {
+            eTag = String.valueOf(dataAtualizacao.toEpochSecond());
+        }
+
+        if (request.checkNotModified(eTag)) {
+            return null;
+        }
+
         FormaPagamento formaPagamento = cadastroFormaPagamento.buscarOuFalhar(formaPagamentoId);
 
         FormaPagamentoModel formaPagamentoModel = formaPagamentoModelAssembler.toModel(formaPagamento);
@@ -77,6 +93,7 @@ public class FormaPagamentoController {
         return ResponseEntity
                 .ok()
                 .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+                .eTag(eTag)
                 .body(formaPagamentoModel);
     }
 
