c6ad867d42fb3bbfb90a690bfb9651b8ec84a325
Paulo Andrino <pauloandrino@yahoo.com.br>
2022-03-29 14:57:17 -0300

    Discoverability and HATEOAS - Adding Usuários Grupos links.

--- src/main/java/com/algaworks/algafoodapi/api/controller/UsuarioGrupoController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/UsuarioGrupoController.java
@@ -1,13 +1,16 @@
 package com.algaworks.algafoodapi.api.controller;
 
+import com.algaworks.algafoodapi.api.AlgaLinks;
 import com.algaworks.algafoodapi.api.assembler.GrupoModelAssembler;
 import com.algaworks.algafoodapi.api.controller.openapi.GrupoControllerOpenApi;
+import com.algaworks.algafoodapi.api.controller.openapi.UsuarioGrupoControllerOpenApi;
 import com.algaworks.algafoodapi.api.model.GrupoModel;
 import com.algaworks.algafoodapi.domain.model.Usuario;
 import com.algaworks.algafoodapi.domain.service.CadastroUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/usuarios/{usuarioId}/grupos")
-public class UsuarioGrupoController {
+public class UsuarioGrupoController implements UsuarioGrupoControllerOpenApi {
 
     @Autowired
     private CadastroUsuarioService cadastroUsuario;
@@ -26,23 +29,41 @@ public class UsuarioGrupoController {
     @Autowired
     private GrupoModelAssembler grupoModelAssembler;
 
+    @Autowired
+    private AlgaLinks algaLinks;
+
+    @Override
     @GetMapping
     public CollectionModel<GrupoModel> listar(@PathVariable Long usuarioId) {
         Usuario usuario = cadastroUsuario.buscarOuFalhar(usuarioId);
 
-        return grupoModelAssembler.toCollectionModel(usuario.getGrupos())
-                .removeLinks();
+        CollectionModel<GrupoModel> gruposModel = grupoModelAssembler.toCollectionModel(usuario.getGrupos())
+                .removeLinks()
+                .add(algaLinks.linkToUsuarioGrupoAssociacao(usuarioId, "associar"));
+
+        gruposModel.getContent().forEach(grupoModel -> {
+            grupoModel.add(algaLinks.linkToUsuarioGrupoDesassociacao(
+                    usuarioId, grupoModel.getId(), "desassociar"));
+        });
+
+        return gruposModel;
     }
 
+    @Override
     @DeleteMapping("/{grupoId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void desassociar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
+    public ResponseEntity<Void> desassociar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
         cadastroUsuario.desassociarGrupo(usuarioId, grupoId);
+
+        return ResponseEntity.noContent().build();
     }
 
+    @Override
     @PutMapping("/{grupoId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void associar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
+    public ResponseEntity<Void> associar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
         cadastroUsuario.associarGrupo(usuarioId, grupoId);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/algaworks/algafoodapi/api/controller/openapi/UsuarioGrupoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/openapi/UsuarioGrupoControllerOpenApi.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -28,7 +29,8 @@ public interface UsuarioGrupoControllerOpenApi {
             @ApiResponse(code = 404, message = "Usuário ou grupo não encontrado",
                     response = Problem.class)
     })
-    void desassociar(
+
+    ResponseEntity<Void> desassociar(
             @ApiParam(value = "ID do usuário", example = "1", required = true)
                     Long usuarioId,
 
@@ -41,7 +43,8 @@ public interface UsuarioGrupoControllerOpenApi {
             @ApiResponse(code = 404, message = "Usuário ou grupo não encontrado",
                     response = Problem.class)
     })
-    void associar(
+
+    ResponseEntity<Void> associar(
             @ApiParam(value = "ID do usuário", example = "1", required = true)
                     Long usuarioId,
 
