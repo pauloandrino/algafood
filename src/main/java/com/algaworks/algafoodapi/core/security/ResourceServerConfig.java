6bdfe458ad86ce15356f184b69151618181b713e
Paulo Andrino <pauloandrino@yahoo.com.br>
2022-05-18 11:37:36 -0300

    Security API - Configuring resource server to use JWT with symmetric key

--- src/main/java/com/algaworks/algafoodapi/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafoodapi/core/security/ResourceServerConfig.java
@@ -1,9 +1,15 @@
 package com.algaworks.algafoodapi.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 @EnableWebSecurity
@@ -16,7 +22,12 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .cors()
                 .and()
-                .oauth2ResourceServer().opaqueToken();
+                .oauth2ResourceServer().jwt();
     }
 
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        var secretKey = new SecretKeySpec("aiusdr237812fas923rn1nrniseh9234a".getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
+    }
 }
